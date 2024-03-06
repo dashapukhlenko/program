8ae77fe35d828d572ff03efe789400e6f71a545c
dashapukhlenko <dpukhlenko@mail.ru>
2024-03-06 14:12:02 +0300

    Добавьте файлы проекта.

---
+++ program.cpp
@@ -0,0 +1,11 @@
+﻿// program.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Привет\n";
+}
+
